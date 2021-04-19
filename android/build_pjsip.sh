818e47c35ac4832a1eb5d2c67e8440c4f95ca401
Martin Juul <code@juul.xyz>
2021-04-19 13:21:33 +0200

    fix build

--- android/build_pjsip.sh
+++ android/build_pjsip.sh
@@ -13,13 +13,16 @@ cat <<EOF >"/tmp/pjsip/pjlib/include/pj/config_site.h"
 #define PJ_CONFIG_ANDROID 1
 #define PJMEDIA_HAS_G729_CODEC 1
 #define PJMEDIA_HAS_G7221_CODEC 1
+#define PJMEDIA_HAS_G711_CODEC 1
 #include <pj/config_site_sample.h>
-#define PJMEDIA_HAS_VIDEO 1
-#define PJMEDIA_AUDIO_DEV_HAS_ANDROID_JNI 0
-#define PJMEDIA_AUDIO_DEV_HAS_OPENSL 1
+#define PJMEDIA_HAS_VIDEO 0
+#define PJMEDIA_AUDIO_DEV_HAS_ANDROID_JNI 1
+#define PJMEDIA_AUDIO_DEV_HAS_OPENSL 0
 #define PJSIP_AUTH_AUTO_SEND_NEXT 0
 #define PJSUA_MAX_CALLS 4
 #define PJ_LOG_MAX_LEVEL 6
+#define PJSIP_UDP_SIZE_THRESHOLD 3000
+#define PJMEDIA_ADVERTISE_RTCP 0
 EOF
 
 cd /tmp/pjsip
